d0cc602f3507cbc95544e85e151748d12f2603a7
opelcrumper <krasimetala@gmail.com>
2015-07-12 22:26:32 +0300

    Remove config users settings

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 
 //Basic configuration of variables for including files
 define('ROOT_PATH', dirname(__FILE__));
-define('HOST_PATH', 'http://xml-parser.dev');
+define('HOST_PATH', '');
 
 //--------- Define database user and password
-define('DB_USER', 'root');
-define('DB_PASS', 'krassis');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 //--------- Set folder for getting structure -----------
 $dir_path = ROOT_PATH . '/animals';
