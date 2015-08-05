c522d43aba3bce79ec91d8b4f2e7572c59474c75
Krasimir Kostadinov <krasimetala@gmail.com>
2015-08-05 14:34:48 +0300

    Added new folder to exclude PHPStorm project and config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Basic configuration of variables for including files
+define('ROOT_PATH', dirname(__FILE__));
+define('HOST_PATH', 'http://quiz.dev');
+
+//--------- Define database user and password
+define('DB_USER', 'root');
+define('DB_PASS', '');
